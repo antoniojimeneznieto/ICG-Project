240854578fdcda0de0edd580f7d0c7a15dcd6037
Jules PERRIN <jules.perrin@epfl.ch>
2023-05-28 18:00:57 +0200

    Better scene

--- Assets/Scripts/PlanetMovement.cs
+++ Assets/Scripts/PlanetMovement.cs
@@ -4,60 +4,87 @@ using UnityEngine;
 
 public class PlanetMovement : MonoBehaviour
 {
-    public float gravitationalConstant = 6.674f;  // Gravitational constant used to calculate the gravitational force.
-    private Rigidbody bodyRigidbody;  // Rigidbody of the current object (planet).
-    public Rigidbody otherBody;  // Rigidbody of the other body (typically, a star or another planet) with which the current object interacts gravitationally.
+    // Variables to store the gravitational constant, the rigidbody component of the object, and another rigidbody that the object will orbit.
+    public float gravitationalConstant = 6.674f;
+    private Rigidbody bodyRigidbody;
+    public Rigidbody otherBody;
 
-    public float orbitalEccentricity = 0.0f;  // The eccentricity of the orbit (0 for a circle, 0-1 for an ellipse).
-    public bool setInitialOrbitalVelocity = true;  // Whether to set the initial orbital velocity.
+    // Variables for the orbital eccentricity and whether to set the initial orbital velocity.
+    public float orbitalEccentricity = 0.0f;
+    public bool setInitialOrbitalVelocity = true;
 
-    public float rotationalSpeed = 360.0f;  // Rotational speed in degrees per second. For Earth, this is roughly 360 degrees per day.
+    // The speed of rotation in degrees per second.
+    public float rotationalSpeed = 360.0f;
 
     void Start()
     {
-        bodyRigidbody = GetComponent<Rigidbody>();  // Get the Rigidbody component attached to this GameObject.
-        if (setInitialOrbitalVelocity)  // Check if we need to set the initial orbital velocity.
+        // Retrieve the rigidbody component and set the initial orbital velocity if required.
+        bodyRigidbody = GetComponent<Rigidbody>();
+        if (setInitialOrbitalVelocity)
         {
-            SetInitialOrbitalVelocity();  // Set the initial orbital velocity.
+            SetInitialOrbitalVelocity();
         }
 
-        // Set the initial rotation speed
-        float rotationSpeedInRadians = rotationalSpeed * Mathf.Deg2Rad;  // Convert rotational speed from degrees to radians.
-        bodyRigidbody.angularVelocity = transform.up * rotationSpeedInRadians;  // Set the initial angular velocity.
+        // Convert the rotational speed to radians and set the angular velocity.
+        float rotationSpeedInRadians = rotationalSpeed * Mathf.Deg2Rad;
+        bodyRigidbody.angularVelocity = transform.up * rotationSpeedInRadians;
     }
 
     private void FixedUpdate()
     {
-        if (otherBody != null)  // Check if there's another body to interact with.
+        // If there's another body specified, apply the gravitational force towards it.
+        if (otherBody != null)
         {
-            ApplyGravity(otherBody);  // Apply the gravitational force to this body.
+            ApplyGravity(otherBody);
         }
     }
 
     private void SetInitialOrbitalVelocity()
     {
+        // If no other body is specified, don't set the orbital velocity.
         if (otherBody == null)
             return;
 
-        float distance = (otherBody.transform.position - transform.position).magnitude;  // Calculate the distance between the bodies.
-        // Calculate the magnitude of the orbital velocity.
-        float orbitalVelocityMagnitude = Mathf.Sqrt(gravitationalConstant * otherBody.mass / distance) * (1 + orbitalEccentricity);
-        // Calculate the direction of the orbital velocity.
-        Vector3 orbitalVelocityDirection = Vector3.Cross((transform.position - otherBody.transform.position).normalized, Vector3.up);
+        // Calculate the distance to the other body and the magnitude of the orbital velocity.
+        float distance = (otherBody.transform.position - transform.position).magnitude;
+        float orbitalVelocityMagnitude = Mathf.Sqrt(gravitationalConstant * otherBody.mass / (distance * (1 + orbitalEccentricity)));
 
-        bodyRigidbody.velocity = orbitalVelocityDirection * orbitalVelocityMagnitude;  // Set the initial velocity of the Rigidbody.
+        // Calculate the direction of the orbital velocity.
+        Vector3 directionToOther = (transform.position - otherBody.transform.position).normalized;
+        Vector3 arbitraryPerpendicular = GetArbitraryPerpendicular(directionToOther);
+        Vector3 orbitalVelocityDirection = Vector3.Cross(directionToOther, arbitraryPerpendicular);
+
+        // Set the velocity of the rigidbody.
+        bodyRigidbody.velocity = orbitalVelocityDirection * orbitalVelocityMagnitude;
+    }
+
+    private Vector3 GetArbitraryPerpendicular(Vector3 vector)
+    {
+        // If the vector is zero, return zero.
+        if (vector == Vector3.zero)
+            return Vector3.zero;
+        // If the vector is not parallel to the up vector, return a vector perpendicular to the input vector and the up vector.
+        else if (vector != Vector3.up && vector != Vector3.down)
+            return Vector3.Cross(vector, Vector3.up);
+        // If the vector is parallel to the up vector, return a vector perpendicular to the input vector and the right vector.
+        else
+            return Vector3.Cross(vector, Vector3.right);
     }
 
     private void ApplyGravity(Rigidbody otherBody)
     {
-        Vector3 forceDirection = otherBody.transform.position - transform.position;  // Calculate the direction of the gravitational force.
-        float distance = forceDirection.magnitude;  // Calculate the distance to the other body.
+        // Calculate the direction of the gravitational force and the distance to the other body.
+        Vector3 forceDirection = otherBody.transform.position - transform.position;
+        float distance = forceDirection.magnitude;
+
         // Calculate the magnitude of the gravitational force.
         float gravitationalForce = gravitationalConstant * ((bodyRigidbody.mass * otherBody.mass) / (distance * distance));
-        Vector3 forceVector = forceDirection.normalized * gravitationalForce;  // Calculate the gravitational force vector.
 
-        bodyRigidbody.AddForce(forceVector);  // Apply the gravitational force to this Rigidbody.
+        // Calculate the gravitational force vector.
+        Vector3 forceVector = forceDirection.normalized * gravitationalForce;
+
+        // Apply the gravitational force to the rigidbody.
+        bodyRigidbody.AddForce(forceVector);
     }
 }
-
 
